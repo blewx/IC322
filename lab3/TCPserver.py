5ace6824c94c1890f4c5bf4be5dfcc8d42a03afb
BLewiy <m263708@usna.edu>
2024-09-05 09:35:17 -0400

    pt2 done... pt3 is shell code

--- lab3/TCPserver.py
+++ lab3/TCPserver.py
@@ -10,7 +10,10 @@ while True:
     sentence = connectionSocket.recv(1024).decode() 
     capitalizedSentence = sentence.upper() 
     message = "Let's make this upper case \n" 
-    print(sentence) 
-    connectionSocket.send(message.encode ()) 
-    connectionSocket.send(capitalizedSentence.encode()) 
+    #m = "<html><body>Hello, world!</body></html>\n"
+    print(sentence)
+    response = """HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: 39\r\n\r\n<html><body>Hello, world!</body></html>"""
+    connectionSocket.send(response.encode())
+    #connectionSocket.send(m.encode())
+    #connectionSocket.send(capitalizedSentence.encode()) 
     connectionSocket.close()
